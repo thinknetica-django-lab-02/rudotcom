64b7d99ac83c0a9951240ca2bd8180e20a0eb395
Alex Troshkov <rudotcom@gmai.com>
2021-03-28 15:13:57 +0300

    Передал переменные page_role и user в include sidebar_nav.html

--- ecommerce/main/views.py
+++ ecommerce/main/views.py
@@ -7,5 +7,6 @@ class BaseView(View):
     def get(self, request):
         context = {
             'turn_on_block': False,
+            'page_role': 'home'
         }
         return render(request, 'base.html', context=context)
