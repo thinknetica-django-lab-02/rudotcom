ad9f1af880b6e69fb0a3e922dd42e4b27175bfb9
Alex Troshkov <rudotcom@gmai.com>
2021-03-31 15:43:47 +0300

    Передал переменные page_role и user в include sidebar_nav.html

--- ecommerce/main/urls.py
+++ ecommerce/main/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from .views import BaseView
+from .views import BaseView, CategoryView, ProfileView, LoginView, LogoutView, RegistrationView, CartView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', BaseView.as_view(), name='base'),
+    path('category/<str:slug>/', CategoryView.as_view(), name='category'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page="/"), name='logout'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('cart/', CartView.as_view(), name='cart'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- ecommerce/main/views.py
+++ ecommerce/main/views.py
@@ -1,12 +1,48 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category
 
 
 class BaseView(View):
 
     def get(self, request):
         context = {
-            'turn_on_block': False,
-            'page_role': 'home'
+            'categories': Category.objects.all(),
+            'page_role': 'goods',
         }
         return render(request, 'base.html', context=context)
+
+
+class CategoryView(View):
+    model = Category
+
+    def get(self, request, *args, **kwargs):
+        slug = kwargs.get('slug')
+        category = Category.objects.get(slug=slug)
+        context = {
+            'category': category,
+            'categories': Category.objects.all(),
+            'page_role': 'goods',
+        }
+        return render(request, 'category.html', context=context)
+
+
+class ProfileView(View):
+    pass
+
+
+class LoginView(View):
+    pass
+
+
+class LogoutView(View):
+    next_page = None
+    pass
+
+
+class RegistrationView(View):
+    pass
+
+
+class CartView(View):
+    pass
